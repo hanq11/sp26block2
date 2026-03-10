83624dd45b5faa50ad3af2fddf3261e8eb13f5b2
hanq11 <>
2026-03-10 15:27:36 +0700

    init

---
+++ sd20306sof3062/src/main/java/com/example/sd20306sof3062/Sd20306sof3062Application.java
@@ -0,0 +1,13 @@
+package com.example.sd20306sof3062;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd20306sof3062Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd20306sof3062Application.class, args);
+    }
+
+}
---
+++ sd20306sof3062/src/test/java/com/example/sd20306sof3062/Sd20306sof3062ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sd20306sof3062;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sd20306sof3062ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
