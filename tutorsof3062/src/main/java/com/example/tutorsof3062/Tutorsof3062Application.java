a3862238a3320ffe96a832dbbd2b2adcff22adb1
hanq11 <hanq11@fpt.edu.vn>
2026-03-26 10:03:05 +0700

    update

---
+++ tutorsof3062/src/main/java/com/example/tutorsof3062/Tutorsof3062Application.java
@@ -0,0 +1,13 @@
+package com.example.tutorsof3062;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorsof3062Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tutorsof3062Application.class, args);
+    }
+
+}
