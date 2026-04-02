c922d9bc077828bfa7afdbb78f1c21075a261aba
hanq11 <hanq11@fpt.edu.vn>
2026-04-02 11:09:36 +0700

    update

--- tutorsof3062/src/main/java/com/example/tutorsof3062/service/TruyenTranhService.java
+++ tutorsof3062/src/main/java/com/example/tutorsof3062/service/TruyenTranhService.java
@@ -1,15 +1,17 @@
 package com.example.tutorsof3062.service;
 
 import com.example.tutorsof3062.entity.TruyenTranh;
+import com.example.tutorsof3062.request.TruyenTranhRequest;
+import com.example.tutorsof3062.response.TruyenTranhResponse;
 
 import java.util.List;
 
 public interface TruyenTranhService {
-    public List<TruyenTranh> hienThi();
+    public List<TruyenTranhResponse> hienThi();
 
-    public void them(TruyenTranh truyenTranh);
+    public void them(TruyenTranhRequest truyenTranhRequest);
 
-    public void sua(TruyenTranh truyenTranh);
+    public void sua(TruyenTranhRequest truyenTranhRequest);
 
     public void xoa(Integer id);
 }
--- tutorsof3062/src/main/java/com/example/tutorsof3062/service/impl/TruyenTranhServiceImpl.java
+++ tutorsof3062/src/main/java/com/example/tutorsof3062/service/impl/TruyenTranhServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.tutorsof3062.service.impl;
 
 import com.example.tutorsof3062.entity.TruyenTranh;
+import com.example.tutorsof3062.exception.ApiException;
 import com.example.tutorsof3062.repository.TruyenTranhRepository;
+import com.example.tutorsof3062.request.TruyenTranhRequest;
+import com.example.tutorsof3062.response.TruyenTranhResponse;
 import com.example.tutorsof3062.service.TruyenTranhService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,22 +17,33 @@ public class TruyenTranhServiceImpl implements TruyenTranhService {
     @Autowired
     TruyenTranhRepository truyenTranhRepository;
     @Override
-    public List<TruyenTranh> hienThi() {
-        return truyenTranhRepository.findAll();
+    public List<TruyenTranhResponse> hienThi() {
+        return truyenTranhRepository
+                .findAll()
+                .stream()
+                .map(TruyenTranhResponse::new)
+                .toList();
     }
 
     @Override
-    public void them(TruyenTranh truyenTranh) {
+    public void them(TruyenTranhRequest truyenTranhRequest) {
+        TruyenTranh truyenTranh = new TruyenTranh();
+        BeanUtils.copyProperties(truyenTranhRequest, truyenTranh);
         truyenTranhRepository.save(truyenTranh);
     }
 
     @Override
-    public void sua(TruyenTranh truyenTranh) {
+    public void sua(TruyenTranhRequest truyenTranhRequest) {
+        TruyenTranh truyenTranh = new TruyenTranh();
+        BeanUtils.copyProperties(truyenTranhRequest, truyenTranh);
         truyenTranhRepository.save(truyenTranh);
     }
 
     @Override
     public void xoa(Integer id) {
+        truyenTranhRepository.findById(id).orElseThrow(
+                () -> new ApiException("Id khong ton tai", "ID_NOT_FOUND")
+        );
         truyenTranhRepository.deleteById(id);
     }
 }
